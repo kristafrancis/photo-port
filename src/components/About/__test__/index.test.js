6c8f9dc8fe96f8dfe634206ae50651d672889578
Krista Francis <kristafrancis08@yahoo.com>
2022-10-18 15:07:48 -0400

    create Nav tests

--- src/components/About/__test__/index.test.js
+++ src/components/About/__test__/index.test.js
@@ -1,7 +1,10 @@
+// __tests__/About.test.js
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
+
+afterEach(cleanup);
 
 describe('About component', () => {
     // First Test
